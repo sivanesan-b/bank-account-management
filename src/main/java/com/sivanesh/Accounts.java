8ff92f01874c10bfdf02caf113c0c5a148c6e752
sivanesh-b <sivanesanb.24@gmail.com>
2025-01-11 17:19:27 +0530

    Minor changes and redundancies have been removed.

--- src/main/java/com/sivanesh/Accounts.java
+++ src/main/java/com/sivanesh/Accounts.java
@@ -24,7 +24,7 @@ public class Accounts{
         }
     }
 
-    List<Transaction> transactions = new ArrayList<Transaction>();
+    List<Transaction> transactions = new ArrayList<>();
 
     public void myTransaction(){
         for(Transaction transaction : transactions){
